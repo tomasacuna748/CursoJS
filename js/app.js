8679fea7003d65f0e8a6ba5486b722bf4ec78190
tomasacuna <tomily748@gmail.com>
2022-07-21 00:03:52 -0300

    arma arrayCarrito pero no se muestra

--- js/app.js
+++ js/app.js
@@ -1,161 +1,124 @@
-let arrayProductos = [];
+const arrayProductos = [];
 let arrayCarrito = [];
 let peluche;
 let total = 0;
 
 class Producto {
-  constructor(nombre, precio, stock) {
+  constructor(id,img,nombre, precio, stock) {
+    this.id = id;
+    this.img = img;
     this.precio = precio;
     this.stock = stock;
     this.nombre = nombre;
   }
 }
 class Carrito {
-  constructor(nombre, precio) {
+  constructor(nombre, precio,img,id,cantidad) {
     this.precio = precio;
     this.nombre = nombre;
+    this.img = img;
+    this.id = id;
+    this.cantidad = cantidad;
   }
 }
 let j = 0;
-arrayProductos[j++] = new Producto("Chanchito", 1000, 9);
-arrayProductos[j++] = new Producto("Elefante", 800, 12);
-arrayProductos[j++] = new Producto("Lobo", 1200, 5);
+const chancho = new Producto(0,"img/Productos/chancho.jpeg","Chanchito", 1000, 9);
+const elefante = new Producto(1,"img/Productos/elefante.jpeg","Elefante", 1100, 12);
+const lobo = new Producto(2,"img/Productos/lobo.jpeg","Lobo", 1200, 5);
+const panda = new Producto(3,"img/Productos/panda.jpeg","Panda", 1500, 7);
+const pulpo = new Producto(4,"img/Productos/pulpo.jpeg","Pulpo", 1600, 5);
+const tigre = new Producto(5,"img/Productos/tigre.jpeg","Tigre", 1400, 6);
 
-function seleccionCarrito(peluche) {
-  switch (peluche) {
-    case 0:
-      if (arrayProductos[peluche].stock < 1) {
-        alert(
-          "no mas Chanchitos disponibles, stocks nuevos cada semana o comunicarse directamente para mayor rapidez"
-        );
-      } else {
-        agregarCarrito(arrayProductos[peluche], arrayCarrito);
+arrayProductos.push(chancho,elefante,lobo,panda,pulpo,tigre);
 
-        alert("Chanchito agregado al carrito");
-        console.log("Chanchito agregado al carrito");
-      }
-      console.log(arrayCarrito);
+for (const producto of arrayProductos){
+    let cardNueva = document.createElement("div");
+    cardNueva.innerHTML = ` 
+    <div>
+        <h3 class="card-izq">${producto.nombre}</h3>
+    </div>
+    <div>
+        <img src= ${producto.img}  alt="Peluche ${producto.nombre}" width="250" height="250">
+    </div>
+    <div class="card-der">
+        <p class="card-precio">${producto.precio}</p>
+        <button id="${producto.id}" type="button" class="btn btn-primary btn-lg boton-comprar">Comprar</button>
+    </div>`;
 
-      /*
-      a = new Carrito (arrayProductos[peluche].nombre,arrayProductos[peluche].precio,arrayProductos[peluche].cantidad+1);
-      flag++;
-      if (a.flag) {        
-        arrayCarrito.push(a);
-      } else {        
-      }
-      arrayCarrito.push(arrayProductos[peluche]);
-      console.log(arrayProductos[peluche].precio); 
-      arrayProductos[peluche].stock--;   
-      console.log("----------------------");     
-      console.log(arrayCarrito.length);
-      */
-      break;
-    case 1:
-      if (arrayProductos[peluche].stock < 1) {
-        alert(
-          "no mas Elefantes disponibles, stocks nuevos cada semana o comunicarse directamente para mayor rapidez"
-        );
-      } else {
-        agregarCarrito(arrayProductos[peluche], arrayCarrito);
+    document.getElementById("cards").append(cardNueva);
+    cardNueva.classList.add("card","tituloObjeto");
 
-        alert("Elefante agregado al carrito");
-        console.log("Elefante agregado al carrito");
-      }
-      console.log(arrayCarrito);
+}
+// eventos armando carrito
 
-      break;
-    case 2:
-      if (arrayProductos[peluche].stock < 1) {
-        alert(
-          "no mas Lobos disponibles, stocks nuevos cada semana o comunicarse directamente para mayor rapidez"
-        );
-      } else {
-        agregarCarrito(arrayProductos[peluche], arrayCarrito);
 
-        alert("Lobo agregado al carrito");
-        console.log("Lobo agregado al carrito");
-      }
-      console.log(arrayCarrito);
 
-      break;
-    default:
-      alert("error de producto");
-      break;
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito"); 
+
+const clickbutton = document.querySelectorAll('.boton-comprar')
+
+clickbutton.forEach(btn => {
+  btn.addEventListener('click', agregarProducto)
+})
+
+
+function agregarProducto(e) {
+  const button = e.target
+	const item = button.closest('.card');
+	const itemNombre = item.querySelector('h3').textContent;
+	const itemPrecio = item.querySelector('p').textContent;
+	const itemID = item.querySelector('button').getAttribute('id');
+	const itemImg = item.querySelector('img').src;
+  const itemCantidad = 1;
+  const nuevoItem = new Carrito(itemNombre,itemPrecio,itemImg,itemID,itemCantidad)
+
+  //console.log(nuevoItem);
+  agregarItem(nuevoItem);
+  //arrayCarrito.push(nuevoItem)
+  console.log(arrayCarrito);
+  
+}
+function agregarItem(nuevoItem){
+	
+	for(let i =0; i < arrayCarrito.length ; i++){
+		if(arrayCarrito[i].nombre.trim() === nuevoItem.nombre.trim()){
+      arrayCarrito[i].cantidad ++;
+      //carritoTotal()
+      return null;
+		}
+	}
+
+	arrayCarrito.push(nuevoItem)
+
+} 
+console.log("llego a la linea 94");//no
+if (arrayCarrito.length === 0){
+  let cardCarrito= document.createElement("div");
+  cardCarrito.innerHTML=`
+  <h2 class="cvacio">Carrito vacio</h2>
+  `;
+  document.getElementById("bcarrito").append(cardCarrito);
+
+}else {
+  for (const cart of arrayCarrito){
+    let cardCarrito =document.createElement("div");
+    cardCarrito.innerHTML = `
+    <div>
+      <div class="tituloObjeto">
+      <img src= ${cart.img}  alt="Peluche ${cart.nombre}" width="100" height="100">
+      </div>
+      <div>
+          <h3 >${cart.nombre}</h3>
+      </div>
+      <div>
+          <p class="card-precio">$ ${cart.precio}</p>
+      </div>
+    </div>
+    <div>
+
+    </div>
+    `;
+    document.getElementById("lista-carrito").append(cardCarrito);
+    cardNueva.classList.add("card","tituloObjeto");
   }
-}
-
-function agregarCarrito(objeto, arrayCarrito) {
-  let n = objeto.nombre;
-  let p = objeto.precio;
-  const a = new Carrito(n, p);
-  arrayCarrito.push(a);
-}
-
-function calculaCompra() {
-  let monto = arrayCarrito.reduce((acc, n) => acc + n.precio, 0);
-  /*for (let index = 0; index < arrayCarrito.length; index++) {
-      monto+= arrayCarrito[index].precio;
-    }*/
-
-  return monto;
-}
-
-function finalizar() {
-  console.log("Los productos del carrito son:");
-  for (let index = 0; index < arrayCarrito.length; index++) {
-    console.log("-" + arrayCarrito[index].nombre);
-  }
-  total = calculaCompra().toFixed(2);
-  alert(`El total de su carrito es: $${total}`);
-  formasDePago();
-}
-
-function formasDePago() {
-  let formaPago = parseInt(prompt("como desea pagar? 1-Contado 2-Credito"));
-  let cuotas;
-
-  if (formaPago === 1) {
-    alert(`Total a pagar: $ ${total}`);
-  } else if (formaPago === 2) {
-    alert("Recargos por cuotas // 2 y 3: 10%  // 4 a 6: 20% // 7 a 12: 35%");
-    cuotas = parseInt(prompt("ingrese cantidad de cuotas(de 2 a 12)"));
-    total = credito(cuotas).toFixed(2);
-    alert(
-      `Total: $ ${total} //  ${cuotas} Cuotas de $ ${(total / cuotas).toFixed(
-        2
-      )}`
-    );
-  } else {
-    alert("error de digito");
-  }
-}
-function credito(cuotas) {
-  switch (cuotas) {
-    case 2:
-    case 3:
-      total = total * 1.1;
-      break;
-    case 4:
-    case 5:
-    case 6:
-      total = total * 1.2;
-      break;
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-      total = total * 1.35;
-      break;
-    default:
-      alert("error cantidad de cuotas");
-  }
-  return total;
-}
-
-function reiniciar() {
-  arrayCarrito = [];
-  console.log("------------------------------");
-  total = 0;
-}
+}//ultimos divs para botones de reiniciar carrito y finalizar compra
---
+++ js/formularios.js
@@ -0,0 +1,16 @@
+/*const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("buscando");
+})
+
+const nombre = document.querySelector("#nombre");
+
+formulario.addEventListener("submit"), validarFormulario;
+
+function validarFormulario(e){
+    e.preventDefault();
+    console.log(nombre.value)
+}
+*/
