d6cbca41ce750bbe16a42701ae773a6a71059c91
tomasacuna <tomily748@gmail.com>
2022-07-27 03:14:23 -0300

    carrito funcionando con multiples idas y vueltas entre httmls

--- js/carrito.js
+++ js/carrito.js
@@ -7,36 +7,42 @@ if (arrayCarrito != null){
     imprimeCarrito();
     
 }else {
+    actualizaVentana();
+    /*
     let cardCarrito= document.createElement("div");
     cardCarrito.innerHTML=`
     <h2 class="cvacio">Carrito vacio</h2>
     `;
-    document.getElementById("lista-carrito").append(cardCarrito);
+    document.getElementById("lista-carrito").append(cardCarrito);*/
 }
 //ultimos divs para botones de reiniciar carrito y finalizar compra
 function imprimeCarrito() {
     for (const cart of arrayCarrito){
-        
+        let pcant =0;
+        pcant= cart.precio*cart.cantidad
         let cardCarrito =document.createElement("div");
         cardCarrito.innerHTML = `
         <div>
             <div>
-            <img src= ../${cart.img}  alt="Peluche ${cart.nombre}" width="125" height="125">
+            <img src= ../${cart.img}  alt="Peluche ${cart.nombre}" width="150" height="150">
             </div>
             <div>
                 <h3 >${cart.nombre}</h3>
             </div>
             <div>
-                <p class="card-precio">$ ${cart.precio}</p>
+                <p class="card-precio">P/u $${cart.precio}</p>
             </div>
             <div>
-                <p>cantidad: ${cart.cantidad}</p>
+                <p class="productos-carrito-cantidad">cantidad: ${cart.cantidad}</p>
+            </div>
+            <div>
+                <p class="card-precio">$ ${pcant}</p>
             </div>
         </div>
         
         `;
         document.getElementById("lista-carrito").append(cardCarrito);
-        cardCarrito.classList.add("card","tituloObjeto");
+        cardCarrito.classList.add("productos-carrito");
         console.log(cart.nombre,cart.precio,cart.cantidad);
         total+= (cart.precio * cart.cantidad);
         console.log(total);
@@ -44,30 +50,36 @@ function imprimeCarrito() {
     
     let cardBotones =document.createElement("div");
     cardBotones.innerHTML = `
-    <button type="submit" class="btn btn-primary" id="vcarrito">Vaciar Carrito</button>
-    <p>$  <span id="precioTotal">0</span></p>
-    <button type="submit" class="btn btn-primary" id="finalizar">Finalizar</button>
-
+    <div>
+        <button type="submit" class="btn btn-primary btn-lg" id="vcarrito">Vaciar Carrito</button>
+    </div>
+    <div>
+        <p class="card-precio">ToTal: $<span id="precioTotal">0</span></p>
+    </div>
+    <div>
+        <button type="submit" class="btn btn-primary btn-lg" id="finalizar">Finalizar</button>
+    </div>
     `;
     document.getElementById("bcarrito").append(cardBotones);
+    cardBotones.classList.add("d-flex","justify-content-around")
+    const botonVaciar = document.getElementById('vcarrito'); 
+    botonVaciar.addEventListener('click',()=>{
+        arrayCarrito.length = 0;
+        localStorage.removeItem('carrito');
+        total=0;
+        actualizaVentana();
+})
     
     
 }
 
 const precioTotal = document.getElementById('precioTotal');
 
-//precioTotal.innerText = arrayCarrito.reduce((acc,prod)=> acc + prod.precio,0);
+precioTotal.innerText = arrayCarrito.reduce((acc,prod)=> acc + prod.precio* prod.cantidad,0);
 
-const botonVaciar = document.getElementById('vcarrito'); 
-botonVaciar.addEventListener('click',()=>{
-    arrayCarrito.length = 0;
-    localStorage.removeItem('carrito');
-    total=0;
-    actualizaVentana();
-})
+
 
 function actualizaVentana() {
-    cardCarrito.innerHTML = "";
     let cardCarrito= document.createElement("div");
     cardCarrito.innerHTML=`
     <h2 class="cvacio">Carrito vacio</h2>
